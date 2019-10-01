2adcb8ed68675c2cdfa02e090d8736c254d59160
liangpeter <liangpeter08@gmail.com>
2019-09-30 21:01:43 -0400

    some upgrade

--- src/components/mainPageContent/mainPageContent.tsx
+++ src/components/mainPageContent/mainPageContent.tsx
@@ -3,6 +3,16 @@ import css from './mainPageContent.css';
 
 export default function MainPageContent() {
     return (
-           <div className={css.body}>Under construction</div>
+        <div className={css.body}>
+            <div className={css.backgroundWhite}>
+            </div>
+            <div className={css.backgroundBlur}>
+            </div>
+            <div className={css.backgroundPic}>
+            </div>
+            <div className={css.content}>
+                Under construction
+            </div>
+        </div>
     );
 }
