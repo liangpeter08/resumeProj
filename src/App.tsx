e18382a13cfbbf9f11794d99b1bdd7da34bbb81a
liangpeter <liangpeter08@gmail.com>
2019-10-12 20:10:01 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import './App.css';
 import Header from './containers/header/header';
-import MainPageContent from './components/mainPageContent/mainPageContent';
+import MainPageContent from './containers/mainPageContent/mainPageContent';
+import ResumeContents from './containers/resumeContents/resumeContent';
 import css from './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 class App extends React.Component {
   render() {
     return (
-      <div className={css.backgroundImg}>
-        <Header />
-        <MainPageContent />
-      </div>
+      <Router>
+        <div className={css.backgroundImg}>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <MainPageContent />
+            </Route>
+            <Route path="/resume">
+              <ResumeContents />
+            </Route>
+          </Switch>
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/ResumeContents/resumeContent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import css from './resumeContent.css';
+
+export default function MainPageContent() {
+    return (
+        <div>Hello</div>
+    );
+}
