cc3ebf298a8632bb6d0bb6b8c08aefade7e5edc5
liangpeter <liangpeter08@gmail.com>
2019-10-13 21:16:56 -0400

    add notes tool to project

--- src/containers/Notes/notes.tsx
+++ src/containers/Notes/notes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './notes.css';
 import { GoogleLogin } from 'react-google-login';
+import CustomButton from '../../components/customButton/customButton';
 
 interface NotesProps {
 
@@ -28,13 +29,14 @@ class Notes extends React.Component<NotesProps, NotesState> {
         return (
             <div className={css.gridTemplate}>
                 <div className={css.controls}>
-                <GoogleLogin
-                    clientId="239128037217-26f613okjt62dqbhh0p3kkdfa7lnfhkl.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={this.successLogin}
-                    onFailure={this.failLogin}
-                    cookiePolicy={'single_host_origin'}
-                />
+                    <CustomButton text={'Create New'} onclick={() => {}} />
+                    <GoogleLogin
+                        clientId="239128037217-26f613okjt62dqbhh0p3kkdfa7lnfhkl.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={this.successLogin}
+                        onFailure={this.failLogin}
+                        cookiePolicy={'single_host_origin'}
+                    />
                 </div>
                 <div></div>
             </div>
