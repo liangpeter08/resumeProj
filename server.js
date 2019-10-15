6e4f64d6a3062a412dc7ae741aa17ba594bb9550
liangpeter <liangpeter08@gmail.com>
2019-10-15 15:41:36 -0400

    fix express routing

--- server.js
+++ server.js
@@ -4,25 +4,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const router = express.Router();
-const router2 = express.Router();
-
-app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
 
 const indexPage = path.join(__dirname, '/dist',  '/index.html');
 
-router.get('/', (req, res) => {
+
+app.use("/view", express.static(path.resolve(path.join(__dirname, '/dist'))));
+
+app.get('/view/*',  (req, res) => {
   res.sendFile(indexPage);
 });
 
-router2.get(/\/.*/, (req, res) => {
-  res.sendFile(indexPage);
-});
-
-app.use('/', router);
-app.use("/view", router2);
-app.use("/api", router);
-
+app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
