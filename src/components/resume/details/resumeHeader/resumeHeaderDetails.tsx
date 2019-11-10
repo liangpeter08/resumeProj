f9b685552a4d570e9d63b9103a2b60b8757e3542
liangpeter <liangpeter08@gmail.com>
2019-11-10 02:21:36 -0500

    flex basis covered

--- src/components/resume/details/resumeHeader/resumeHeaderDetails.tsx
+++ src/components/resume/details/resumeHeader/resumeHeaderDetails.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-// import css from './resumeHeader.css';
+import css from './resumeHeaderDetails.css';
 
 interface ResumeHeaderProps {
 };
@@ -8,7 +8,14 @@ interface ResumeHeaderProps {
 const resumeHeaderDetails : FunctionComponent<ResumeHeaderProps> = ({}: ResumeHeaderProps) => {
 
     return (
-        <div>this is some extra detail information about myself</div>
+        <div className={css.wrapper}>
+            <div>My Legal name is Yi Chao Peter Liang</div>
+            <div>Here is my website: liangpeter.com</div>
+            <div>Also I have a old resume website here: https://ycpliang.github.io/ResumeProj/</div>
+            <div>Feel free to reach out to my email: liangpeter08@gmail.com</div>
+            <div>I have several github account, liangpeter08 is my primary github account as of now</div> 
+            <div>My phone number is included as of now, might be removed in future revisions. It would preferred if you text.</div>
+        </div>
     );
 };
 
